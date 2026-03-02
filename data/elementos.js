f964aa5a83864715233eeca1dd311e9685068b77
simuladorprototipos-rgb <simuladorprototipos@gmail.com>
2026-03-01 19:59:40 -0600

    Update elementos.js with atomic mass and new elements
    Added atomic mass to existing elements and included additional elements.

--- data/elementos.js
+++ data/elementos.js
@@ -5,6 +5,7 @@ const elementos = [
         nombre: "Hidrógeno",
         grupo: 1,
         periodo: 1,
+        masa: 1.008,
         tipo: "no-metal"
     },
     {
@@ -32,6 +33,14 @@ const elementos = [
         tipo: "metal-alcalinoterreo",
 
     },
+
+
+
+
+
+
+
+
     {
         numero: 11,
         simbolo: "Na",
@@ -39,5 +48,281 @@ const elementos = [
         grupo: 1,
         periodo: 3,
         tipo: "metal-alcalino"
-    }
+    },
+    {
+        numero: 19,
+        simbolo: "K",
+        nombre: "Potasio",
+        grupo: 1,
+        periodo: 4,
+        tipo: "metal-alcalino"
+    },
+    {
+        numero: 37,
+        simbolo: "Rb",
+        nombre: "Rubidio",
+        grupo: 1,
+        periodo: 5,
+        tipo: "metal-alcalino"
+    },
+    {
+        numero: 55,
+        simbolo: "Cs",
+        nombre: "Cesio",
+        grupo: 1,
+        periodo: 6,
+        tipo: "metal-alcalino"
+    },
+    {
+        numero: 87,
+        simbolo: "Fr",
+        nombre: "Francio",
+        grupo: 1,
+        periodo: 7,
+        tipo: "metal-alcalino"
+    },
+    {
+        numero: 57,
+        simbolo: "La",
+        nombre: "Lantano",
+        grupo: 4,
+        periodo: 6,
+        tipo: "lactanidos"
+    },
+    {
+        numero: 58,
+        simbolo: "Ce",
+        nombre: "Cerio",
+        grupo: 5,
+        periodo: 6,
+        tipo: "lactanidos"
+    },
+    {
+        numero: 59,
+        simbolo: "Pr",
+        nombre: "Praseodimio",
+        grupo: 6,
+        periodo: 6,
+        tipo: "lactanidos"
+    },
+        {
+        numero: 60,
+        simbolo: "Nd",
+        nombre: "Neodimio",
+        grupo: 7,
+        periodo: 6,
+        tipo: "lactanidos"
+    },
+    {
+        numero: 61,
+        simbolo: "Pm",
+        nombre: "Prometio",
+        grupo: 8,
+        periodo: 6,
+        tipo: "lactanidos"
+    },
+    {
+        numero: 62,
+        simbolo: "Sm",
+        nombre: "Samario",
+        grupo: 9,
+        periodo: 6,
+        tipo: "lactanidos"
+    },
+        {
+        numero: 63,
+        simbolo: "Eu",
+        nombre: "Europio",
+        grupo: 10,
+        periodo: 6,
+        tipo: "lactanidos"
+    },
+    {
+        numero: 64,
+        simbolo: "Gd",
+        nombre: "Gadolinio",
+        grupo: 11,
+        periodo: 6,
+        tipo: "lactanidos"
+    },
+    {
+        numero: 65,
+        simbolo: "Tb",
+        nombre: "Terbio",
+        grupo: 12,
+        periodo: 6,
+        tipo: "lactanidos"
+    },
+    {
+        numero: 66,
+        simbolo: "Dy",
+        nombre: "Disprosio",
+        grupo: 13,
+        periodo: 6,
+        tipo: "lactanidos"
+    },
+    {
+        numero: 67,
+        simbolo: "Ho",
+        nombre: "Holmio",
+        grupo: 14,
+        periodo: 6,
+        tipo: "lactanidos"
+    },
+    {
+        numero: 68,
+        simbolo: "Er",
+        nombre: "Ersio",
+        grupo: 15,
+        periodo: 6,
+        tipo: "lactanidos"
+    },
+        {
+        numero: 69,
+        simbolo: "Tm",
+        nombre: "Tulio",
+        grupo: 16,
+        periodo: 6,
+        tipo: "lactanidos"
+    },
+    {
+        numero: 70,
+        simbolo: "Yb",
+        nombre: "Iterbio",
+        grupo: 17,
+        periodo: 6,
+        tipo: "lactanidos"
+    },
+    {
+        numero: 71,
+        simbolo: "Lu",
+        nombre: "Lutecio",
+        grupo: 18,
+        periodo: 6,
+        tipo: "lactanidos"
+    },
+    {
+        numero: 89,
+        simbolo: "Ac",
+        nombre: "Actinio",
+        grupo: 4,
+        periodo: 7,
+        tipo: "actinidos"
+    },
+    {
+        numero: 90,
+        simbolo: "Th",
+        nombre: "Torio",
+        grupo: 5,
+        periodo: 7,
+        tipo: "actinidos"
+    },
+    {
+        numero: 91,
+        simbolo: "Pa",
+        nombre: "Protactinio",
+        grupo: 6,
+        periodo: 7,
+        tipo: "actinidos"
+    },
+        {
+        numero: 92,
+        simbolo: "U",
+        nombre: "Uranio",
+        grupo: 7,
+        periodo: 7,
+        tipo: "actinidos"
+    },
+    {
+        numero: 93,
+        simbolo: "Np",
+        nombre: "Neptunio",
+        grupo: 8,
+        periodo: 7,
+        tipo: "actinidos"
+    },
+    {
+        numero: 94,
+        simbolo: "Pu",
+        nombre: "Plutonio",
+        grupo: 9,
+        periodo: 7,
+        tipo: "actinidos"
+    },
+        {
+        numero: 95,
+        simbolo: "Am",
+        nombre: "Americio",
+        grupo: 10,
+        periodo: 7,
+        tipo: "actinidos"
+    },
+    {
+        numero: 96,
+        simbolo: "Cm",
+        nombre: "Curio",
+        grupo: 11,
+        periodo: 7,
+        tipo: "actinidos"
+    },
+    {
+        numero: 97,
+        simbolo: "Bk",
+        nombre: "Berquelio",
+        grupo: 12,
+        periodo: 7,
+        tipo: "actinidos"
+    },
+    {
+        numero: 98,
+        simbolo: "Cf",
+        nombre: "Californio",
+        grupo: 13,
+        periodo: 7,
+        tipo: "actinidos"
+    },
+    {
+        numero: 99,
+        simbolo: "Es",
+        nombre: "Einstenio",
+        grupo: 14,
+        periodo: 7,
+        tipo: "actinidos"
+    },
+    {
+        numero: 100,
+        simbolo: "Fm",
+        nombre: "Fermio",
+        grupo: 15,
+        periodo: 7,
+        tipo: "actinidos"
+    },
+    {
+        numero: 101,
+        simbolo: "Md",
+        nombre: "Mendelevio",
+        grupo: 16,
+        periodo: 7,
+        tipo: "actinidos"
+    },
+    {
+        numero: 102,
+        simbolo: "No",
+        nombre: "Nobelio",
+        grupo: 17,
+        periodo: 7,
+        tipo: "actinidos"
+    },
+    {
+        numero: 103,
+        simbolo: "Lr",
+        nombre: "Laurencio",
+        grupo: 18,
+        periodo: 7,
+        tipo: "actinidos"
+    },
+
+  
+ 
+
 ];
